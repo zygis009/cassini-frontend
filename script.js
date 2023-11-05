188a274a491644aefc0ed6036b830b2b3f0e402b
karol-202 <karoljurski1@gmail.com>
2023-11-05 09:35:27 +0100

    Use Leaflet map

--- script.js
+++ script.js
@@ -35,47 +35,23 @@ function removeInput(btn) {
 }
 
 /**
- * Zooming stuff below...
+ * Map stuff below...
  */
+const map = L.map('map', {
+    maxBounds: [[0, 0], [1, 1]],
+}).setView([0, 0], 13);
+map.setMinZoom(map.getBoundsZoom(map.options.maxBounds)); // TODO Fix
+// TODO Get rid of absolute paths
+const layerTrueColor = L.imageOverlay('file:///home/karol/dev/contests/cassini23/cassini-frontend/resources/Sentinel-2_L2A_True_color.jpg', [[0, 0], [1, 1]], {opacity: 1});
+const layerB = L.imageOverlay('file:///home/karol/dev/contests/cassini23/cassini-frontend/resources/Sentinel-2_L2A_Custom_script.jpg', [[0, 0], [1, 1]], {opacity: 1});
+const layerC = L.imageOverlay('file:///home/karol/dev/contests/cassini23/cassini-frontend/resources/DEM_COPERNICUS_30_Topographic.jpg', [[0, 0], [1, 1]], {opacity: 1});
 
-const zoomableContainer = document.getElementById('zoomable-container');
-const zoomableImage = document.getElementById('zoomable-image');
-const svgRoad = document.getElementById('svg-road');
+const baseLayers = {
+    "True color": layerTrueColor,
+    "Layer B": layerB,
+    "Layer C": layerC,
+};
+L.control.layers(baseLayers).addTo(map);
+layerA.addTo(map);
 
-let scale = 1;
-const initialStrokeWidth = 1; // Set your desired initial stroke width here
 
-
-zoomableContainer.addEventListener('wheel', (e) => {
-    e.preventDefault();
-    const scaleFactor = 0.1;
-
-    if (e.deltaY > 0) {
-        scale -= scaleFactor;
-    } else {
-        scale += scaleFactor;
-    }
-
-    // Limit the minimum and maximum zoom levels
-    scale = Math.min(3, Math.max(1, scale));
-
-    // Calculate the mouse position relative to the image container
-    const mouseX = e.clientX - zoomableContainer.getBoundingClientRect().left;
-    const mouseY = e.clientY - zoomableContainer.getBoundingClientRect().top;
-    const transformOriginX = (mouseX / zoomableContainer.clientWidth) * 100;
-    const transformOriginY = (mouseY / zoomableContainer.clientHeight) * 100;
-
-    // Apply the zoom transformation to the image
-    zoomableImage.style.transform = `scale(${scale})`;
-    zoomableImage.style.transformOrigin = `${transformOriginX}% ${transformOriginY}%`;
-
-    // Calculate the adjusted stroke width based on the current scale
-    const adjustedStrokeWidth = initialStrokeWidth / scale;
-
-    // Apply the adjusted stroke width to the SVG road
-
-    svgRoad.style.transform = `scale(${scale})`;
-    svgRoad.style.strokeWidth = `${adjustedStrokeWidth}px`;
-    svgRoad.style.strokeWidth = `${adjustedStrokeWidth}px`;
-    svgRoad.style.transformOrigin = `${transformOriginX}% ${transformOriginY}%`;
-});
